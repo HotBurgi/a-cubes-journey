1bd5118308ec72e2fadc7fb1a453280d3228f125
D4rknezSl1me <xiancangelli@gmail.com>
2025-05-29 09:27:43 +0200

    collisions and map change updates

--- cubes-jurney/scripts/player.gd
+++ cubes-jurney/scripts/player.gd
@@ -4,6 +4,11 @@ var wait_timer := 0.0
 var rel_pos = Vector2(5,5)
 var curr_room = Vector2(0, 1)
 
+#var map = GlobalVariables.maps[GlobalVariables[curr_room.x][curr_room.y]]
+var map = GlobalVariables.maps[0]
+
+var curr_tile = map[rel_pos.x][rel_pos.y]
+
 const MOVE_INTERVAL := 0.2  # Seconds between moves (adjust as needed)
 
 func _process(delta: float) -> void:
@@ -11,29 +16,42 @@ func _process(delta: float) -> void:
 	
 	if wait_timer <= 0.0:
 		if Input.is_action_pressed("ui_right") and checkCollision(rel_pos.x+1, rel_pos.y):
+			if curr_tile == 6:
+				rel_pos = Vector2(0,0) # Metti le coordinate giuste
+				#aggiorna var map copia incollando il commento sopra (lascialo commentato anche qui)
 			movement.x += 32
 			rel_pos.x += 1
 			wait_timer = MOVE_INTERVAL
 		elif Input.is_action_pressed("ui_left") and checkCollision(rel_pos.x-1, rel_pos.y):
+			if curr_tile == 6: # Metti giusto
+				rel_pos = Vector2(0,0) # Metti le coordinate giuste
+				#aggiorna var map copia incollando il commento sopra (lascialo commentato anche qui)
 			movement.x -= 32
 			rel_pos.x -= 1
 			wait_timer = MOVE_INTERVAL
 		elif Input.is_action_pressed("ui_down") and checkCollision(rel_pos.x, rel_pos.y+1):
+			if curr_tile == 6: # Metti giusto
+				rel_pos = Vector2(0,0) # Metti le coordinate giuste
+				#aggiorna var map copia incollando il commento sopra (lascialo commentato anche qui)
 			movement.y += 32
 			rel_pos.y += 1
 			wait_timer = MOVE_INTERVAL
 		elif Input.is_action_pressed("ui_up") and checkCollision(rel_pos.x, rel_pos.y-1):
+			if curr_tile == 6: # Metti giusto
+				rel_pos = Vector2(0,0) # Metti le coordinate giuste
+				#aggiorna var map copia incollando il commento sopra (lascialo commentato anche qui)
 			movement.y -= 32
 			rel_pos.y -= 1
 			wait_timer = MOVE_INTERVAL
 
 	position += movement
+	curr_tile = map[rel_pos.x][rel_pos.y]
 	wait_timer -= delta
 
 func checkCollision(x, y):
-	#var map = GlobalVariables.maps[GlobalVariables[curr_room.x][curr_room.y]]
-	var map = GlobalVariables.maps[0]
+	#map = GlobalVariables.maps[GlobalVariables[curr_room.x][curr_room.y]]
+	map = GlobalVariables.maps[0]
 	# check collision
-	if(map[x][y] != 0 and map[x][y]!=2):   # non dovremmo tenere 2 per le porte, le porte metti 0, non serve a nulla
+	if(map[x][y] != 0 and !(map[x][y] >= 6 and map[x][y] <= 9)):
 		return false
 	return true
