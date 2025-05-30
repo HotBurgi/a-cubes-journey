5f0b7aec912586f424ac87e75602f146f15deecc
D4rknezSl1me <xiancangelli@gmail.com>
2025-05-30 21:12:33 +0200

    relative position and map updates

--- cubes-jurney/scripts/global_variables.gd
+++ cubes-jurney/scripts/global_variables.gd
@@ -11,7 +11,7 @@ var MAP_EIGHT = ROOM_SIDE * 5 # 5 rooms
 # 1 spawn
 # Bisogna mettere i numeri per tipo di stanza in base alla posizione nell'array di mappe in globalvars
 
-var MAP = [	[-1, 1, -1],
+var MAP = [	[0, 1, 0],
 			[0, 0, 0],
 			[0, 0, 0],
 			[0, 0, 0],
--- cubes-jurney/scripts/player.gd
+++ cubes-jurney/scripts/player.gd
@@ -16,21 +16,25 @@ func _process(delta: float) -> void:
 	
 	if wait_timer <= 0.0:
 		if Input.is_action_pressed("ui_right") and checkCollision(rel_pos.x+1, rel_pos.y):
-			if curr_tile == 6:
-				rel_pos = Vector2(0,0) # Metti le coordinate giuste
-				#aggiorna var map copia incollando il commento sopra (lascialo commentato anche qui)
+			if curr_tile == 7:
+				rel_pos.x = -1
+				curr_room.y += 1
+				#var map = GlobalVariables.maps[GlobalVariables[curr_room.x][curr_room.y]]
+				var map = GlobalVariables.maps[0]
 			movement.x += 32
 			rel_pos.x += 1
 			wait_timer = MOVE_INTERVAL
 		elif Input.is_action_pressed("ui_left") and checkCollision(rel_pos.x-1, rel_pos.y):
-			if curr_tile == 6: # Metti giusto
-				rel_pos = Vector2(0,0) # Metti le coordinate giuste
-				#aggiorna var map copia incollando il commento sopra (lascialo commentato anche qui)
+			if curr_tile == 9: # Metti giusto
+				rel_pos.x = 11
+				curr_room.y -= 1
+				#var map = GlobalVariables.maps[GlobalVariables[curr_room.x][curr_room.y]]
+				var map = GlobalVariables.maps[0]
 			movement.x -= 32
 			rel_pos.x -= 1
 			wait_timer = MOVE_INTERVAL
 		elif Input.is_action_pressed("ui_down") and checkCollision(rel_pos.x, rel_pos.y+1):
-			if curr_tile == 6: # Metti giusto
+			if curr_tile == 8: # Metti giusto
 				rel_pos = Vector2(0,0) # Metti le coordinate giuste
 				#aggiorna var map copia incollando il commento sopra (lascialo commentato anche qui)
 			movement.y += 32
@@ -44,11 +48,14 @@ func _process(delta: float) -> void:
 			rel_pos.y -= 1
 			wait_timer = MOVE_INTERVAL
 
+	print(rel_pos.x, " - ", rel_pos.y)
 	position += movement
 	curr_tile = map[rel_pos.x][rel_pos.y]
 	wait_timer -= delta
 
 func checkCollision(x, y):
+	x = int(x) % 11
+	y = int(y) % 11
 	#map = GlobalVariables.maps[GlobalVariables[curr_room.x][curr_room.y]]
 	map = GlobalVariables.maps[0]
 	# check collision
