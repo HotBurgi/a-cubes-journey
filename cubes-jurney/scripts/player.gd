3844cd64507689a56addca1c70057806b7b3733a
Chri <xiancangelli@gmail.com>
2025-06-04 18:08:51 +0200

    Added one way tiles, special action tiles and fixed boss room collisions

--- cubes-jurney/scripts/player.gd
+++ cubes-jurney/scripts/player.gd
@@ -7,15 +7,25 @@ var curr_room = Vector2(0, 1)
 var map = GlobalVariables.maps[GlobalVariables.MAP[curr_room.x][curr_room.y]]
 
 var curr_tile = map[rel_pos.x][rel_pos.y]
+var last_key_pressed = ""
 
 const MOVE_INTERVAL := 0.2  # Seconds between moves (adjust as needed)
+var special_delay_triggered := false # Crea la variabile e assegna implicitamente il type
 
 func _process(delta: float) -> void:
+	if curr_tile == 5 and not special_delay_triggered:
+		wait_timer = 2
+		special_delay_triggered = true
+		return
+	elif curr_tile != 5:
+		special_delay_triggered = false
+		
 	var movement = Vector2.ZERO
 	var room_to_check = curr_room
 	
 	if wait_timer <= 0.0:
 		if Input.is_action_pressed("ui_right") and checkCollision(rel_pos.x+1, rel_pos.y, room_to_check):
+			last_key_pressed = "ui_right"
 			if curr_tile == 7:
 				room_to_check.y += 1
 			if checkCollision(rel_pos.x+1, rel_pos.y, room_to_check):
@@ -26,6 +36,7 @@ func _process(delta: float) -> void:
 				rel_pos.x += 1
 				wait_timer = MOVE_INTERVAL
 		elif Input.is_action_pressed("ui_left"):
+			last_key_pressed = "ui_left"
 			if curr_tile == 9:
 				room_to_check.y -= 1
 			if checkCollision(rel_pos.x-1, rel_pos.y, room_to_check):
@@ -36,6 +47,7 @@ func _process(delta: float) -> void:
 				rel_pos.x -= 1
 				wait_timer = MOVE_INTERVAL
 		elif Input.is_action_pressed("ui_down"):
+			last_key_pressed = "ui_down"
 			if curr_tile == 8:
 				room_to_check.x -= 1
 			if checkCollision(rel_pos.x, rel_pos.y+1, room_to_check):
@@ -46,6 +58,7 @@ func _process(delta: float) -> void:
 				rel_pos.y += 1
 				wait_timer = MOVE_INTERVAL
 		elif Input.is_action_pressed("ui_up"):
+			last_key_pressed = "ui_up"
 			if curr_tile == 6:
 				room_to_check.x += 1
 			if checkCollision(rel_pos.x, rel_pos.y-1, room_to_check):
@@ -68,7 +81,9 @@ func checkCollision(x, y, croom):
 	map = GlobalVariables.maps[GlobalVariables.MAP[croom.x][croom.y]]
 	# check collision
 	print(map[x][y])
-	if(map[x][y] == 1):
+	if(map[x][y] == 5 and curr_tile == 5):
+		return true
+	if((map[x][y] == 1) or (map[x][y] == 5 and last_key_pressed != "ui_up") or (curr_tile == 5 and last_key_pressed == "ui_up")):
 		return false
 	return true
 
