3c9ecac121b80ac86335a158c19a74ba756a7976
Chri <xiancangelli@gmail.com>
2025-06-04 17:42:07 +0200

    wip - map collision and decors

--- cubes-jurney/scripts/player.gd
+++ cubes-jurney/scripts/player.gd
@@ -12,48 +12,65 @@ const MOVE_INTERVAL := 0.2  # Seconds between moves (adjust as needed)
 
 func _process(delta: float) -> void:
 	var movement = Vector2.ZERO
+	var room_to_check = curr_room
 	
 	if wait_timer <= 0.0:
-		if Input.is_action_pressed("ui_right") and checkCollision(rel_pos.x+1, rel_pos.y):
+		if Input.is_action_pressed("ui_right") and checkCollision(rel_pos.x+1, rel_pos.y, room_to_check):
 			if curr_tile == 7:
-				rel_pos.x = -1
-				curr_room.y += 1
-			movement.x += 32
-			rel_pos.x += 1
-			wait_timer = MOVE_INTERVAL
-		elif Input.is_action_pressed("ui_left") and checkCollision(rel_pos.x-1, rel_pos.y):
+				room_to_check.y += 1
+			if checkCollision(rel_pos.x+1, rel_pos.y, room_to_check):
+				if curr_tile == 7:
+					rel_pos.x = -1
+					curr_room.y += 1
+				movement.x += 32
+				rel_pos.x += 1
+				wait_timer = MOVE_INTERVAL
+		elif Input.is_action_pressed("ui_left"):
 			if curr_tile == 9:
-				rel_pos.x = 11
-				curr_room.y -= 1
-			movement.x -= 32
-			rel_pos.x -= 1
-			wait_timer = MOVE_INTERVAL
-		elif Input.is_action_pressed("ui_down") and checkCollision(rel_pos.x, rel_pos.y+1):
+				room_to_check.y -= 1
+			if checkCollision(rel_pos.x-1, rel_pos.y, room_to_check):
+				if curr_tile == 9:
+					rel_pos.x = 11
+					curr_room.y -= 1
+				movement.x -= 32
+				rel_pos.x -= 1
+				wait_timer = MOVE_INTERVAL
+		elif Input.is_action_pressed("ui_down"):
 			if curr_tile == 8:
-				rel_pos.y = -1
-				curr_room.x -= 1 
-			movement.y += 32
-			rel_pos.y += 1
-			wait_timer = MOVE_INTERVAL
-		elif Input.is_action_pressed("ui_up") and checkCollision(rel_pos.x, rel_pos.y-1):
+				room_to_check.x -= 1
+			if checkCollision(rel_pos.x, rel_pos.y+1, room_to_check):
+				if curr_tile == 8:
+					rel_pos.y = -1
+					curr_room.x -= 1 
+				movement.y += 32
+				rel_pos.y += 1
+				wait_timer = MOVE_INTERVAL
+		elif Input.is_action_pressed("ui_up"):
 			if curr_tile == 6:
-				rel_pos.y = 11
-				curr_room.x += 1
-			movement.y -= 32
-			rel_pos.y -= 1
-			wait_timer = MOVE_INTERVAL
+				room_to_check.x += 1
+			if checkCollision(rel_pos.x, rel_pos.y-1, room_to_check):
+				if curr_tile == 6:
+					rel_pos.y = 11
+					curr_room.x += 1
+				movement.y -= 32
+				rel_pos.y -= 1
+				wait_timer = MOVE_INTERVAL
 			
 	position += movement
 	curr_tile = map[rel_pos.x][rel_pos.y]
 	var map = GlobalVariables.maps[GlobalVariables.MAP[curr_room.x][curr_room.y]]
 	wait_timer -= delta
 
-func checkCollision(x, y):
-	x = int(x) % 11
-	y = int(y) % 11
+func checkCollision(x, y, croom):
+	x = posmod(x, 11)
+	y = posmod(y, 11)
 	
-	map = GlobalVariables.maps[GlobalVariables.MAP[curr_room.x][curr_room.y]]
+	map = GlobalVariables.maps[GlobalVariables.MAP[croom.x][croom.y]]
 	# check collision
-	if(map[x][y] != 0 and !(map[x][y] >= 6 and map[x][y] <= 9)):
+	print(map[x][y])
+	if(map[x][y] == 1):
 		return false
 	return true
+
+func posmod(n: int, m: int) -> int:
+	return ((n % m) + m) % m
